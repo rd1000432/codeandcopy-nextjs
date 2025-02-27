385c01ca31affa23f036471e53005c1c41e21a1e
Ronan DIneen <ronandineen@Ronans-Air.fritz.box>
2025-02-27 16:07:39 +0100

    enable visual editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { getStoryblokApi } from "@/storyblok";
 import { StoryblokStory } from "@storyblok/react/rsc";
+import { draftMode } from "next/headers";
 
 const fetchHomePage = async () => {
+  const { isEnabled } = await draftMode();
   const client = getStoryblokApi();
   const response = await client.getStory(`home`, {
-    version: process.env.NODE_ENV === "development" ? "draft" : "published",
+    version: process.env.NODE_ENV === "development" || isEnabled
+     ? "draft"
+     : "published",
     resolve_relations: "recommended_services.services",
   });
   return response.data.story;
